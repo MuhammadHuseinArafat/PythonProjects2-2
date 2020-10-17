61705d8c32ebe94a6cd3c591399248e528b7b353
Nofia Nisa <nofiaanis29@gmail.com>
2020-10-17 13:02:30 +0700

    Initial commit

---
+++ praktikum4.py
@@ -0,0 +1,32 @@
+#Praktikum 4 Pertama
+
+#nomor satu
+print(type(10))
+
+#nomor dua
+print(type(10.1918))
+
+#nomor tiga
+print(type("Hello world"))
+
+#nomor empat
+print(type(True))
+
+#praktikum 4 Kedua
+
+#nomor dua
+x = 10
+print(type(x))
+y = 20
+print(type(y))
+print(type(x+y))
+
+#nomor lima
+a = 10
+p = y = x = z = a
+print(a)
+print(z)
+print(x)
+print(y)
+print(p)
+
